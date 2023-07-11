a346955ed3bc832793d5276f6da126ecbf23b893
Francine <francine.ssouza@outlook.com>
2023-07-11 01:03:54 -0300

    Exercicios ruby

---
+++ loop.rb
@@ -0,0 +1,20 @@
+#criar um contador
+#precisar de uma variável
+
+count = 0
+
+#precisa de um loop de execução
+loop do
+    
+    puts count
+    #condição -> se(if) contador não for igual 150
+
+    if count == 150
+        break
+    end
+
+    #incremento -> somar 1
+
+    count +=1
+
+end
